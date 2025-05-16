ab771d2ac5aae3de9b7c5d371cd2c048dc99f01b
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-05-16 16:40:56 +0300

    Routing Config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './Layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { MainLayoutComponent } from './Layouts/main-layout/main-layout.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login',
+      },
+    ],
+  },
+
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        title: 'Home',
+      },
+      {
+        path: '**',
+        component: NotFoundComponent,
+        title: 'Error',
+      },
+    ],
+  },
+];
