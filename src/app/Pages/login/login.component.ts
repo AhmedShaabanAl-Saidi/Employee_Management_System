1c4892a8d93a54dbd01ee66dcd24e870688f6377
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-05-16 18:24:54 +0300

    Login Form design

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null),
+    password: new FormControl(null),
+  });
 }
