e00513b351ba6acb5d87481a32061c4d83b32d87
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-05-16 21:54:21 +0300

    home design

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  addNewEmployee: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    image: new FormControl(null, [Validators.required]),
+    salary: new FormControl(null, [Validators.required, Validators.min(0)]),
+  });
 }
