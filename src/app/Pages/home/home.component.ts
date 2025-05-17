fbeffe803964511c0f995516ede6b41288f0af10
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-05-17 16:33:07 +0300

    Save data To LocalStorage

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -6,7 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Employee } from '../../Core/interfaces/employee';
 import { EmployeesService } from '../../Core/services/employees/employees.service';
 import { ToastrService } from 'ngx-toastr';
@@ -16,7 +16,7 @@ import { SearchPipe } from '../../Shared/pipes/search.pipe';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule,SearchPipe,FormsModule],
+  imports: [ReactiveFormsModule, CommonModule, SearchPipe, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -24,61 +24,77 @@ export class HomeComponent implements OnInit {
   employees: Employee[] = [];
   editingId: number | null = null;
   searchText: string = '';
+  private localStorageKey = 'employees';
 
+  // Form with validation
   addNewEmployee: FormGroup = new FormGroup({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
     image: new FormControl('', [Validators.required]),
-    salary: new FormControl(null, [Validators.required, Validators.min(0),Validators.pattern(/^\d+$/)]),
+    salary: new FormControl(null, [
+      Validators.required,
+      Validators.min(0),
+      Validators.pattern(/^\d+$/),
+    ]),
   });
 
   constructor(
     private empService: EmployeesService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
   ngOnInit(): void {
-    this.getEmployees();
+    this.loadEmployees();
   }
 
-  getEmployees(): void {
-    this.empService.getAll().subscribe({
-      next: (res) => {
-        this.employees = res;
-      },
-    });
+  loadEmployees(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Try loading from localStorage first
+      const localData = localStorage.getItem(this.localStorageKey);
+      if (localData) {
+        this.employees = JSON.parse(localData);
+      } else {
+        this.empService.getAll().subscribe({
+          next: (res) => {
+            this.employees = res;
+            this.saveToLocalStorage();
+          },
+        });
+      }
+    } else {
+      this.employees = [];
+    }
+  }
+
+  // save To LocalStorage
+  saveToLocalStorage(): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.employees));
   }
 
   SubmitAddNewEmployee(): void {
     if (this.addNewEmployee.invalid) return;
 
-    const empData = this.addNewEmployee.value;
+    const empData: Employee = {
+      ...this.addNewEmployee.value,
+      id: this.editingId ?? this.getNextId(),
+    };
 
     if (this.editingId === null) {
-      // Create
-      this.empService.add(empData).subscribe({
-        next: () => {
-          this.getEmployees();
-          this.resetForm();
-          this.toastr.success(
-            'A new employee has been added successfully.',
-            'Add New Employee'
-          );
-        },
-      });
+      // Add new
+      this.employees.push(empData);
+      this.toastr.success('Added successfully!', 'Add Employee');
     } else {
-      // Update
-      this.empService.update(this.editingId, empData).subscribe({
-        next: () => {
-          this.getEmployees();
-          this.resetForm();
-          this.toastr.success(
-            'update employee successfully',
-            'Update Employee'
-          );
-        },
-      });
+      // Update existing
+      const index = this.employees.findIndex(
+        (emp) => emp.id === this.editingId
+      );
+      if (index !== -1) this.employees[index] = empData;
+      this.toastr.success('Updated successfully!', 'Update Employee');
     }
+
+    this.saveToLocalStorage();
+    this.resetForm();
   }
 
   editEmployee(emp: Employee): void {
@@ -87,15 +103,9 @@ export class HomeComponent implements OnInit {
   }
 
   deleteEmployee(id: number): void {
-    this.empService.delete(id).subscribe({
-      next: () => {
-        this.getEmployees();
-        this.toastr.success(
-          'A employee has been delete successfully.',
-          'Delete Employee'
-        );
-      },
-    });
+    this.employees = this.employees.filter((emp) => emp.id !== id);
+    this.saveToLocalStorage();
+    this.toastr.success('Deleted successfully!', 'Delete Employee');
   }
 
   resetForm(): void {
@@ -106,23 +116,25 @@ export class HomeComponent implements OnInit {
   // sweetAlert
   confirmBox(id: number) {
     Swal.fire({
-      title: 'Are you sure want to remove?',
-      text: 'You will not be able to recover this file!',
+      title: 'Are you sure?',
+      text: "You won't be able to undo this!",
       icon: 'warning',
       showCancelButton: true,
       confirmButtonText: 'Yes, delete it!',
       cancelButtonText: 'No, keep it',
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire(
-          'Deleted!',
-          'Your imaginary file has been deleted.',
-          'success'
-        );
         this.deleteEmployee(id);
-      } else if (result.dismiss === Swal.DismissReason.cancel) {
-        Swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error');
+        Swal.fire('Deleted!', 'Employee has been removed.', 'success');
+      } else {
+        Swal.fire('Cancelled', 'Your employee is safe :)', 'error');
       }
     });
+  }
+
+  // Get next available ID
+  private getNextId(): number {
+    if (this.employees.length === 0) return 1;
+    return Math.max(...this.employees.map((emp) => emp.id ?? 0)) + 1;
   }
 }
